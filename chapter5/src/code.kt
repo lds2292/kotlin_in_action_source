88256b1e2b20ebfa7f5c3f44175dc1b7708c7a2f
leedeoksoo <lds2292@naver.com>
2022-02-08 22:49:12 +0900

    feat : chapter5 updated

--- chapter5/src/code.kt
+++ chapter5/src/code.kt
@@ -1,3 +1,5 @@
+import kotlin.math.max
+
 //data class Person(val name: String, val age: Int)
 //fun findTheOldest(people: List<Person>){
 //    var maxAge = 0
@@ -52,20 +54,113 @@
 //    val errors = listOf("403 Forbidden", "404 Not Found")
 //    printMessageWithPrefix(errors, "Error:")
 //}
+//
+//fun printProblemCounts(responses: Collection<String>){
+//    var clientErrors = 0
+//    var serverErrors = 0
+//    responses.forEach{
+//        if (it.startsWith("4")){
+//            clientErrors++
+//        } else if (it.startsWith("5")){
+//            serverErrors++
+//        }
+//    }
+//    println("$clientErrors client errors, $serverErrors server errors")
+//}
+//fun main() {
+//    val responses = listOf("200 OK", "418 I'm a teapot", "500 Internal Server Error")
+//    printProblemCounts(responses)
+//}
 
-fun printProblemCounts(responses: Collection<String>){
-    var clientErrors = 0
-    var serverErrors = 0
-    responses.forEach{
-        if (it.startsWith("4")){
-            clientErrors++
-        } else if (it.startsWith("5")){
-            serverErrors++
-        }
-    }
-    println("$clientErrors client errors, $serverErrors server errors")
-}
-fun main() {
-    val responses = listOf("200 OK", "418 I'm a teapot", "500 Internal Server Error")
-    printProblemCounts(responses)
-}
+// TODO : Person::age는 다음 람다식을 더 간략하게 표현한 것이다
+//class Person(val name:String, val age:Int)
+//fun main() {
+//    val getAge = { person: Person -> person.age }
+//    val getAge2 = Person::age
+//}
+
+
+// TODO : 멤버 참조를 자유롭게 바꿔 쓸 수 있다
+//class Person(val name:String, val age:Int)
+//fun main() {
+//    val people = listOf(Person("Alice", 29), Person("Bob", 31))
+//    people.maxByOrNull(Person::age)
+//    people.maxByOrNull { p -> p.age }
+//    people.maxByOrNull { it.age }
+//}
+
+// TODO : 최상위에 선언된 함수나 프로퍼티를 참조할 수도 있다
+//fun salute() = println("Salute!")
+//fun main() {
+//    run(::salute)
+//}
+
+
+// TODO : 람다가 인자라 여럿인 다른 함수에게 작업을 위임하는 경우 람다를 정의하지 않고 직접 위임 함수에 대한 참조를 제공하면 편리
+//class Person(val name:String, val age:Int)
+//val action = { person:Person, message: String -> sendEmail(person, message)}
+//fun sendEmail(person: Person, message: String) : String {
+//    return "to ${person.name}, your age ${person.age} and message : $message"
+//}
+//val nextAction = ::sendEmail
+
+// TODO : 생성자 참조
+//data class Person(val name:String, val age:Int)
+//
+//fun main() {
+//    val createPerson = ::Person
+//    val p = createPerson("Alice", 29)
+//    println(p)
+//}
+
+// TODO : 확장 함수도 멤버 함수와 똑같은 방식으로 참조할 수 있다
+//data class Person(val name:String, val age:Int)
+//fun Person.isAdult() = age >= 21
+//fun main() {
+//    val predicate = Person::isAdult
+//    val p = Person("Alice", 29)
+//    predicate(p)
+//}
+
+// TODO : 바운드 멤버 참조
+//data class Person(val name:String, val age:Int)
+//fun main() {
+//    val p = Person("Dmitry", 34)
+//    val personAgeFunction = Person::age
+//    println(personAgeFunction(p))
+//
+//    val dmitryAgeFunction = p::age
+//    println(dmitryAgeFunction())
+//}
+
+// TODO : filter 함수
+//data class Person(val name:String, val age:Int)
+//fun main() {
+//    val list = listOf(1,2,3,4)
+//    println(list.filter{it % 2 == 0})
+//
+//    val people = listOf(Person("Alice", 29), Person("Bob",31))
+//    println(people.filter { it.age > 30 })
+//}
+
+// TODO : map 함수
+//data class Person(val name:String, val age:Int)
+//fun main() {
+//    val list = listOf(1,2,3,4)
+//    println(list.map { it * it })
+//
+//    val people = listOf(Person("Alice", 29), Person("Bob",31))
+//
+//    println(people.map { it.name })
+//    println(people.map(Person::name))
+//
+//    println(people.filter { it.age > 30 }.map(Person::name))
+//
+//    people.filter { it.age == people.maxByOrNull(Person::age)!!.age }
+//
+//    val maxAge = people.maxByOrNull { it.age }!!.age
+//    people.filter { it.age == maxAge }
+//
+//    val numbers = mapOf(0 to "zero", 1 to "one")
+//    println(numbers.mapValues { it.value.uppercase() })
+//}
